09b120824198852a8c978471ef44242dda6ce21f
rostnrol <112760457+rostnrol@users.noreply.github.com>
2023-11-19 22:49:09 +0400

    Update Scaler.cs

--- Scripts/Scaler.cs
+++ Scripts/Scaler.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Scaler : MonoBehaviour
 {
-    [SerializeField] private float scaleSpeed = 0.3f;
+    [SerializeField] private float _scaleSpeed = 0.3f;
 
     private Vector3 _scaleChange;
 
-    void Start()
+    private void Start()
     {
-        _scaleChange = new Vector3(scaleSpeed, scaleSpeed, scaleSpeed);
+        _scaleChange = new Vector3(_scaleSpeed, _scaleSpeed, _scaleSpeed);
     }
 
-    void Update()
+    private void Update()
     {
         transform.localScale += _scaleChange * Time.deltaTime;
     }
